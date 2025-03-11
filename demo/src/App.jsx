c3d25222cc345ae5d30f0138edc82f501654efdf
elbaheleng <elbaheleng@gmail.com>
2025-03-11 15:47:03 +0530

    react introduction

---
+++ demo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  //js code
+ 
+
+  return (
+    //output - html
+    <>
+      <h1>React</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et dicta nam quasi pariatur nemo vero fugit eius aspernatur velit, eos deserunt odio? Laboriosam aliquam illum eaque recusandae. Sunt, adipisci porro?</p>
+    </>
+  )
+}
+
+export default App
