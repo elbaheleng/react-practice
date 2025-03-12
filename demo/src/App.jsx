4dd2a66ea0a7981daad85bb4eb10e7607eaee76a
elbaheleng <elbaheleng@gmail.com>
2025-03-12 15:50:29 +0530

    react adding css,js create components

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,15 +1,39 @@
-
 import './App.css'
+import style from '../src/myStyle.module.css'
+import Example from './Example'
+import Random from './Random'
 
 function App() {
   //js code
- 
+  let name = 'Elba'
+  let name1
+  const clickfun = () => {
+    alert ('button clicked')
+  }
+  const clickfun2 = (emp) => {
+    console.log(emp);
+    
+  }
+  const dispname = (name1) => {
+    n.innerHTML = name1 + ' is ' + n.innerHTML// directly changing DOM, not recommeneded
+  }
+
 
   return (
     //output - html
     <>
-      <h1>React</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et dicta nam quasi pariatur nemo vero fugit eius aspernatur velit, eos deserunt odio? Laboriosam aliquam illum eaque recusandae. Sunt, adipisci porro?</p>
+      <h1> {name} is learning React</h1>
+      <h3 id = "n">learning React</h3>
+
+      <p style = {{ color : 'red', backgroundColor : 'yellow' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et dicta nam quasi pariatur nemo vero fugit eius aspernatur velit, eos deserunt odio? Laboriosam aliquam illum eaque recusandae. Sunt, adipisci Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et dicta nam quasi pariatur nemo vero fugit eius aspernatur velit, eos deserunt odio? Laboriosam aliquam illum eaque recusandae. Sunt, adipisci porro?</p>
+      <p className={style.reactStyle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi excepturi culpa veniam architecto nesciunt inventore molestiae, perspiciatis error cum dignissimos animi rerum sapiente reiciendis repellat aut saepe quasi quisquam id.</p>
+      <p className={style.cssStyle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi excepturi culpa veniam architecto nesciunt inventore molestiae, perspiciatis error cum dignissimos animi rerum sapiente reiciendis repellat aut saepe quasi quisquam id.</p>
+      <button className={style.button} onClick={clickfun}>Get Alert</button>
+      <button className={style.button} onClick={() => {clickfun2('Peter')}}>Get in console</button>
+      <button className={style.button} onClick={() => {dispname('Elba')}}>Display name</button>
+
+    <Example/>
+    <Random/>
     </>
   )
 }
