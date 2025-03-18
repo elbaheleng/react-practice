d8671fae1313375d3e3a4efdd878464d54493ae7
elbaheleng <elbaheleng@gmail.com>
2025-03-18 15:48:35 +0530

    simple interest calculator

--- demo/src/Register.jsx
+++ demo/src/Register.jsx
@@ -1,40 +1,70 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Register() {
 
     const [userDetails, setUserDetails] = useState({
-        name : "",
-        email : "",
-        passwd : ""
+        name: "",
+        email: "",
+        passwd: ""
     })
-    
+
+    const [newArray, setNewArray] = useState([
+        { username: 'Max', age: 27 },
+        { username: 'Neel', age: 37 },
+        { username: 'Mikki', age: 27 },
+        { username: 'Manu', age: 37 }
+    ])
+
     const getName = (e) => {
-        setUserDetails({...userDetails, name : e.target.value})
+        setUserDetails({ ...userDetails, name: e.target.value })
     }
     const getEmail = (e) => {
-        setUserDetails({...userDetails, email : e.target.value})
+        setUserDetails({ ...userDetails, email: e.target.value })
     }
     const getPassword = (e) => {
-        setUserDetails({...userDetails, passwd : e.target.value})
+        setUserDetails({ ...userDetails, passwd: e.target.value })
     }
 
-  return (
-    <form  className="mt-5 w-50 border shadow p-5 ms-5">
-        <h1 className='text-center mb-4'>Register Form</h1>
-        <div className="mb-5">
-            <input type="text" placeholder='Full Name'className='form-control' onChange={(e) => getName(e)} />
-        </div>
-        <div className="mb-5">
-            <input type="text" placeholder='Email'className='form-control' onChange={(e) => getEmail(e)} />
-        </div>
-        <div className="mb-5">
-            <input type="text" placeholder='Password'className='form-control' onChange={(e) => getPassword(e)} />
-        </div>
-        <div className="mb-5">
-            <button type='button' className='btn btn-primary' onClick={()=>{console.log(userDetails)}}>Submit</button>
-        </div>
+    return (
+        <>
+            <form className="mt-5 w-75 border shadow p-5 ms-5">
+                <h1 className='text-center mb-4'>Register Form</h1>
+                <div className="mb-5">
+                    <input type="text" placeholder='Full Name' className='form-control' onChange={(e) => getName(e)} />
+                </div>
+                <div className="mb-5">
+                    <input type="text" placeholder='Email' className='form-control' onChange={(e) => getEmail(e)} />
+                </div>
+                <div className="mb-5">
+                    <input type="text" placeholder='Password' className='form-control' onChange={(e) => getPassword(e)} />
+                </div>
+                <div className="mb-5">
+                    <button type='button' className='btn btn-primary' onClick={() => { console.log(userDetails) }}>Submit</button>
+                </div>
 
-    </form>
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sl.No.</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        newArray.map((item, index) => (
+                            <tr>
+                                <td>{index + 1}</td>
+                                <td>{item.username}</td>
+                                <td>{item.age}</td>
+                            </tr>
+                        ))
+                    }
 
-  )
+                </tbody>
+            </table>
+        </>
+
+    )
 }
--- simpleinterest/src/App.jsx
+++ simpleinterest/src/App.jsx
@@ -1,27 +1,90 @@
 import { useState } from 'react'
 import './App.css'
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [principal, setPrincipal] = useState("")
+  const [rate, setRate] = useState("")
+  const [year, setYear] = useState("")
+  const [interest, setInterest] = useState(0)
+  const [isPrincipal, setIsPrincipal] = useState(true)
+  const [isRate, setIsRate] = useState(true)
+  const [isYear, setIsYear] = useState(true)
 
+  const validate = (e) => {
+    const {name , value} = e.target // object destructuring - gatting name and value only from e.target
+    if (!!value.match('^[0-9]*$')){
+      if (name == 'principal'){
+        setPrincipal(value)
+        setIsPrincipal(true)
+      }
+      else if (name == 'rate'){
+        setRate(value)
+        setIsRate(true)
+      }
+      else {
+        setYear(value)
+        setIsYear(true)
+      }
+    }
+    else {
+      if (name == 'principal'){
+        setPrincipal(value)
+        setIsPrincipal(false)
+      }
+      else if (name == 'rate'){
+        setRate(value)
+        setIsRate(false)
+      }
+      else {
+        setYear(value)
+        setIsYear(false)
+      }
+    }  
+  }
+   const handleReset = () => {
+    setPrincipal("")
+    setRate("")
+    setYear("")
+    setInterest(0)
+    setIsPrincipal(true)
+    setIsRate(true)
+    setIsYear(true)
+   }
+
+   const calculate = () => {
+    setInterest((principal*rate*year)/100)
+   }
   return (
     <>
       <div className=' row'>
-      <div className='col-4'></div>
-      <form className='bg-light p-5 col-4 mt-4 rounded'>
-          <h1>Simple Interest App</h1>
+        <div className='col-4'></div>
+        <form className='bg-light p-4 col-4 mt-2 rounded'>
+          <h1 id='heading'>Simple Interest App</h1>
           <p>Calculate your simple interest easily</p>
           <div id='displ' className='text-center text-white rounded p-3 mb-3'>
-            <h3>₹ 0</h3>
+            <h3>₹ {interest}</h3>
             <p>Total simple interest</p>
           </div>
-          <input type="text" className='form-control mb-3' placeholder='₹ Principal Amount' />
-          <input type="text" className='form-control mb-3' placeholder='Rate of Interest (%)' />
-          <input type="text" className='form-control mb-3' placeholder='Year (Yr)' />
-          <div className='d-flex flex-row gap-3 justify-content-between'>
-            <button className='btn btn-success btn-sm p-3 fs-6 col-6' > CALCULATE</button>
-            <button className='btn btn-outline-primary btn-sm p-3 fs-6 col-6'>RESET</button>
+          <div className="mb-3">
+            <TextField id="outlined-basic" value={principal} name='principal' label="₹ Principal Amount" variant="outlined" className='w-100' onChange={(e) => validate(e)} />
+              {!isPrincipal && <span className='text-danger'>*Invalid Input</span>}
+          </div>
+          <div className="mb-3">
+            <TextField id="outlined-basic" value={rate} name='rate' label="Rate of Interest (%)" variant="outlined" className='w-100'  onChange={(e) => validate(e)}  />
+            {!isRate && <span className='text-danger'>*Invalid Input</span>}          </div>
+          <div className="mb-3">
+            <TextField id="outlined-basic" value={year} name='year' label="Year (Yr)" variant="outlined" className='w-100'  onChange={(e) => validate(e)}  />
+            {!isYear && <span className='text-danger'>*Invalid Input</span>}          </div>
+          <div className=''>
+            <Stack spacing={1} direction="row">
+              <Button variant="contained" color="success" className='w-50'  size="large" onClick={calculate}>CALCULATE</Button>
+              <Button variant="outlined"  className='w-50'  size="large" onClick={handleReset}>RESET</Button>
+            </Stack>
+
           </div>
 
         </form>
