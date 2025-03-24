550f135784f8eec083eb7f525db8a72985a3e823
elbaheleng <elbaheleng@gmail.com>
2025-03-24 15:50:27 +0530

    react json server created

---
+++ mediaplayer/backend/index.js
@@ -0,0 +1,23 @@
+//import json-server
+const jsonserver = require("json-server")
+
+// create server
+const mediaPlayerServer = jsonserver.create()
+
+//create a middleware to parse the json data
+const middleware = jsonserver.defaults()
+
+//set path for the data to store
+const routes = jsonserver.router('db.json')
+
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(use)
+
+// set server port
+PORT = 4001 || process.env.PORT //process.env.PORT will work only when deployed
+
+// server run
+mediaPlayerServer.listen(PORT, () => {
+    console.log(`Port is ${PORT}`);
+    
+})
